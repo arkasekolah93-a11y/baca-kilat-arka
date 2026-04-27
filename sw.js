7ac1b9fe1c30a1eb439aed400793897dec2c6909
arkasekolah93-a11y <arkasekolah93@gmail.com>
2026-04-27 22:03:41 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'baca-arka-v1';
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+// Aktifkan Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Bypass fetch (agar tidak bentrok dengan Speech API)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
